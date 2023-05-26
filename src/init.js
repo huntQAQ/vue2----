3b4b74fd852f10e6e709541dba0d88cbaa53dc36
hunt <2728035271@qq.com>
2023-05-26 14:52:19 +0800

    1

--- src/init.js
+++ src/init.js
@@ -6,7 +6,7 @@ export function initMixin(Vue){
         console.log(options)
         let vm = this
         vm.$options = options  // 给vm实例的options属性赋值options
-        // 初始化状态
+        // 初始化状态1
         initState(vm)
     }
 }
